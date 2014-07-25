1a2a3605430ba714441971ddd66155985890e4e2
Iwood53 <Iwood1350@gmail.com>
2014-07-25 19:37:39 -0400

    Added suport for up to base 36
    added converting numbers greater than 9 and less than 35 into letters

--- BaseX.py
+++ BaseX.py
@@ -10,6 +10,9 @@ def is_number(s):
 
 
 def convert(base_10_int, conv_to_base):
+    num_to_alpha = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J'}
+    num_to_alpha.update({20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T'})
+    num_to_alpha.update({30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z'})
     place_holder = [1]
     running_max = conv_to_base - 1
 
@@ -36,14 +39,19 @@ def convert(base_10_int, conv_to_base):
                 final_num.append(0)
                 will_continue = False
 
+    for index, num in enumerate(final_num):
+        if num > 9:
+            print 'above 9'
+            final_num[index] = num_to_alpha[num]
     final_num_string = "".join(str(x) for x in final_num)
     return final_num_string
 
+if __name__ == "__main__":
+    if len(sys.argv) == 3 and is_number(sys.argv[1]) and is_number(sys.argv[2]):
+        print(convert(int(sys.argv[1]), int(sys.argv[2])))
+    else:
+        print("Usage: BaseX.py <convert from base 10 num> <to base x>")
 
-if len(sys.argv) == 3 and is_number(sys.argv[1]) and is_number(sys.argv[2]):
-    print(convert(int(sys.argv[1]), int(sys.argv[2])))
-else:
-    print("Usage: BaseX.py <convert from base 10 num> <to base x>")
 
 
 
